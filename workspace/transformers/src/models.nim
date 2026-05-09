c2f6163ea21007be8b6bad55478edd6144740935
Mamy Ratsimbazafy <mamy_github@numforge.co>
2026-05-09 23:22:09 +0200

    test_model_loading: compiles

--- workspace/transformers/src/models.nim
+++ workspace/transformers/src/models.nim
@@ -5,6 +5,32 @@
 #   * Apache v2 license (license terms in the root directory or at http://www.apache.org/licenses/LICENSE-2.0).
 # at your option. This file may not be copied, modified, or distributed except according to those terms.
 
-import ./models/all_interfaces
+import ./models/all_reexports
+export all_reexports
 
-export all_interfaces
+import std/json
+import std/os
+import std/tables
+import workspace/libtorch
+
+proc loadModel*(modelPath: string, device = kCPU): Model =
+  # Pass the compile-time -> runtime boundary
+  # and make the var {.compiletime.} a const at runtime
+  const registry = static(ModelRegistry)
+
+  let cfg = modelPath.joinPath("config.json").parseFile()
+  let archs = cfg["architectures"]
+
+  if archs.len == 0:
+    raise newException(ValueError, "[ttt] No architectures found in config.json")
+
+  if archs.len > 1:
+    raise newException(ValueError, "[ttt] Multiple architectures not supported")
+
+  let arch = archs[0].getStr()
+
+  if not registry.hasKey(arch):
+    raise newException(ValueError, "[ttt] Unknown architecture: " & arch)
+
+  let loader = registry[arch]
+  loader(modelPath, device)
---
+++ workspace/transformers/src/models/all_reexports.nim
@@ -0,0 +1,11 @@
+# Imports
+# ----------------------------------------
+import ./all_interfaces
+
+import ./qwen3
+
+# Reexports
+# -----------------------------------------
+export all_interfaces
+
+export qwen3
